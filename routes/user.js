7a12758de6c546b2564bbaa53376de07ffc66253
Nikhil Walia <49777890+nikhilwalia23@users.noreply.github.com>
2022-07-20 15:49:25 +0530

    Update user.js
    Remove login welcome route for testing

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const {buyPackage, getUser} = require("../controllers/user");
 var router=express.Router();
 router.post("/signup",singUp);
 router.post("/login",login);
-router.get("/welcome",isLogin,welcome);
+router.get("/welcome",welcome);
 router.post("/buypackage",isLogin,buyPackage);
 router.get("/user/:UserId",getUser);
 module.exports=router;

154873261e9b579e3e9fd712ae33f67b92473aa8
Nikhil Walia <nikhilwalia363@gmail.com>
2022-08-04 14:57:09 +0530

    Packages Routes Added

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
 var express=require("express");
 const { login,singUp,isLogin, welcome } = require("../controllers/auth");
 const {getUser, updateUser} = require("../controllers/user");
+const {showPackages, buyPackage} =require("../controllers/package")
 var router=express.Router();
 router.post("/signup",singUp);
 router.post("/login",login);
 router.get("/welcome",isLogin,welcome);
 router.get("/user/:UserId",getUser);
 router.put("/user/:UserId",isLogin,updateUser);
+router.get("/package/showpackage",isLogin,showPackages);
+router.post("/package/buypackage",isLogin,buyPackage)
 module.exports=router;

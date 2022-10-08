4e43f870dd8d829c71e70ebca6d8076f6e3ca4d9
Nikhil Walia <49777890+nikhilwalia23@users.noreply.github.com>
2022-10-08 23:23:50 +0530

    jush checking cookies issues

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_ADDRESS).then(res => console.log("Database Co
 //MidleWare Stuff
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors({origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://relaxed-alfajores-1445cf.netlify.app','https://api.razorpay.com'],credentials: true}));
+app.use(cors({origin: ['http://localhost:3000/cardetails','http://localhost:3000', 'http://127.0.0.1:3000','https://relaxed-alfajores-1445cf.netlify.app','https://api.razorpay.com'],credentials: true}));
 
 
 //Imports Routes From all Files

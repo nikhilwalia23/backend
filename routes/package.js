3cc2d7118b4b9b0bae9358acf03bfab75e7070bd
Nikhil Walia <nikhilwalia363@gmail.com>
2022-10-10 10:21:33 +0530

    nothing major

--- routes/package.js
+++ routes/package.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const {buyPackage, showPackages, showAllPackages}=require("../controllers/package")
 const {isLogin} = require("../controllers/auth");
 router.post("/package/buypackage",isLogin,buyPackage);
-router.get("/package/showpackage",isLogin,showPackages);
+router.post("/package/showpackage",isLogin,showPackages);
 router.get("/package/showallpackage",showAllPackages);
 module.exports=router;
